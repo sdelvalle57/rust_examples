8b3139c0ffc733a2f14bab3af9c698367179fc64
sdelvalle57 <sdelvalle57@gmail.com>
2021-09-24 11:58:55 -0500

    Trait as state machine

--- traits_state/src/main.rs
+++ traits_state/src/main.rs
@@ -1,15 +1,22 @@
+use std::fmt::Debug; 
 
-trait State { }
 
 
+trait State: Debug { 
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+}
+
+#[derive(Debug)]
 struct Post {
     state: Option<Box<dyn State>>,
     content: String
 }
 
-struct Draft {}
 
-impl State for Draft {}
 
 impl Post {
     pub fn new() -> Post {
@@ -24,8 +31,76 @@ impl Post {
     pub fn content(&self) -> &str {
         ""
     }
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review());
+        }
+    }
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
 }
 
+#[derive(Debug)]
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview{})
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+#[derive(Debug)]
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+#[derive(Debug)]
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    //Inits as Draft
+    let mut post = Post::new();
+
+    println!("Post state after creating {:?}", post.state); //Draft
+
+    post.add_text("I ate arepa for breakfast today");
+    assert_eq!("", post.content());
+
+    //Changes to PendingReview
+    post.request_review();
+    println!("Post state after request review {:?}", post.state); //pendingReview
+
+    //approves review
+    post.approve();
+    println!("Post state after approve {:?}", post.state); //Published
+
+    println!("Post content {}", post.content);
 }
